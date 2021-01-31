bbcaa1ad93deb2ae811f218ba9fed9e0c5e90e6f
tsaketh <tsaketh1107@gmail.com>
2021-01-31 17:00:23 +0530

    Updates

---
+++ precache-manifest.86d73b3c80230b7426b2a5030cfd0dfb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb0d8eb1887ca7a6d9bcb09ad313e6f",
+    "url": "/my-recommended-movies/index.html"
+  },
+  {
+    "revision": "8d36ca219583d842d25d",
+    "url": "/my-recommended-movies/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b6726e54710ce3ca1d0c",
+    "url": "/my-recommended-movies/static/css/main.69996292.chunk.css"
+  },
+  {
+    "revision": "8d36ca219583d842d25d",
+    "url": "/my-recommended-movies/static/js/2.1957c274.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-recommended-movies/static/js/2.1957c274.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6726e54710ce3ca1d0c",
+    "url": "/my-recommended-movies/static/js/main.6698395e.chunk.js"
+  },
+  {
+    "revision": "d59c19b42e7bd135f102",
+    "url": "/my-recommended-movies/static/js/runtime-main.b9be0459.js"
+  },
+  {
+    "revision": "3a9eb038ee0f66e422249c91ef466872",
+    "url": "/my-recommended-movies/static/media/loading.3a9eb038.png"
+  }
+]);
